abe2fba024a42a3070499c9f6cda9ebe8abc153b
HaydenWadsworth <hayydenn@mail.com>
2019-06-19 22:27:58 +0100

    Skeleton built

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src(['scss/**/*.scss', 'node_modules/bootstrap/scss/bootstrap.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+    return gulp.src(['scss/**/*.scss', 'node_modules/bootstrap/scss/bootstrap.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('./css/'))
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('scss/**/*.scss',['styles']);
+    gulp.watch('scss/main.scss', gulp.series('styles'));
 });
